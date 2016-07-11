ae975106ef4ae25880b85807dfc16726f95e2ab6
我叫李铁柱 <lk0219@foxmail.com>
2016-07-11 14:20:01 +0800

    order fragment

--- app/src/main/java/zju/kevin/mytest/fragment/MenuFragment.java
+++ app/src/main/java/zju/kevin/mytest/fragment/MenuFragment.java
@@ -2,9 +2,7 @@ package zju.kevin.mytest.fragment;
 
 import zju.kevin.mytest.R;
 
-import android.app.Activity;
 import android.app.ListFragment;
-//import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,21 +10,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
-import android.widget.ArrayAdapter;
-
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class MenuFragment extends ListFragment {
 
     private String TAG = MenuFragment.class.getName();
-    //private ListView list ;
-    //private SimpleAdapter adapter;
     /**
      * @描述 在onCreateView中加载布局
      * */
@@ -40,11 +32,10 @@ public class MenuFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final String [] from =new String[] {"name", "price"};
-        final int[] to = new int[] {R.id.dish_name, R.id.dish_price};
+        final String [] from =new String[] {"name", "price","img"};
+        final int[] to = new int[] {R.id.dish_name, R.id.dish_price, R.id.dish_img};
         Log.i(TAG, "--------onCreate");
 
-        String[] list = {"dish 1","dish 2","dish 3","dish 4","dish 5"};
         SimpleAdapter adapter=new SimpleAdapter(this.getActivity(),
                 getData(), R.layout.menu_item, from, to);
         this.setListAdapter(adapter);
@@ -70,21 +61,16 @@ public class MenuFragment extends ListFragment {
     private List<Map<String, Object>> getData() {
         List<Map<String ,Object>> list = new ArrayList<Map<String,Object>>();
 
-//        for (int i = 0; i < strs.length; i++) {
-//            Map<String, Object> map = new HashMap<String, Object>();
-//            map.put("title", strs[i]);
-//            list.add(map);
-//        }
-
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("name", "饺子");
         map.put("price", 20.00);
+        map.put("img",R.drawable.ic_launcher);
         list.add(map);
         map=new HashMap<String, Object>();
         map.put("name", "noodles");
         map.put("price", 100.00);
+        map.put("img",R.drawable.ic_launcher);
         list.add(map);
-        //map.clear();
 
         return list;
     }
--- app/src/main/java/zju/kevin/mytest/fragment/OrderFragment.java
+++ app/src/main/java/zju/kevin/mytest/fragment/OrderFragment.java
@@ -4,40 +4,21 @@ import zju.kevin.mytest.R;
 
 import android.app.ListFragment;
 import android.os.Bundle;
-//import android.support.v4.app.Fragment;
-//import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-//import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.SimpleAdapter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-
-//public class OrderFragment extends ListFragment {
-//
-//	@Override
-//	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//			Bundle savedInstanceState) {
-//		return inflater.inflate(R.layout.order_fragment, container, false);
-//	}
-//
-//	@Override
-//	public void onActivityCreated(Bundle savedInstanceState) {
-//		// TODO Auto-generated method stub
-//		super.onActivityCreated(savedInstanceState);
-//		((TextView)getView().findViewById(R.id.tvTop)).setText("订单");
-//	}
-//}
+import java.util.List;
+import java.util.Map;
 
 public class OrderFragment extends ListFragment {
 
 	private String TAG = OrderFragment.class.getName();
-	//private ListView list ;
-	//private SimpleAdapter adapter;
 	/**
 	 * @描述 在onCreateView中加载布局
 	 * */
@@ -52,40 +33,44 @@ public class OrderFragment extends ListFragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "--------onCreate");
-		String[] list = {"order 1","order 2","order 3","order 4","order 5"};
-		this.setListAdapter(new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1, list));
+        String[] from=new String[] {"order_id", "name", "contact", "order_time"};
+        int[] to = new int[] {R.id.order_id, R.id.name, R.id.contact, R.id.order_time};
+		SimpleAdapter adapter=new SimpleAdapter(this.getActivity(),
+				getData(), R.layout.order_item, from, to);
+
+        this.setListAdapter(adapter);
 
 	}
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-
-		System.out.println(l.getChildAt(position));
-		HashMap<String, Object> view= (HashMap<String, Object>) l.getItemAtPosition(position);
-		System.out.println(view.get("title").toString()+"+++++++++title");
-
-
-		Toast.makeText(getActivity(), TAG+l.getItemIdAtPosition(position), Toast.LENGTH_LONG).show();
-		System.out.println(v);
-		System.out.println(position);
+//		super.onListItemClick(l, v, position, id);
+//
+//		System.out.println(l.getChildAt(position));
+//		HashMap<String, Object> view= (HashMap<String, Object>) l.getItemAtPosition(position);
+//		System.out.println(view.get("title").toString()+"+++++++++title");
+//
+//
+//		Toast.makeText(getActivity(), TAG+l.getItemIdAtPosition(position), Toast.LENGTH_LONG).show();
+//		System.out.println(v);
+//		System.out.println(position);
 
 
 	}
 
-//    private List<? extends Map<String, ?>> getData(String[] strs) {
-//        List<Map<String ,Object>> list = new ArrayList<Map<String,Object>>();
-//
-//        for (int i = 0; i < strs.length; i++) {
-//            Map<String, Object> map = new HashMap<String, Object>();
-//            map.put("title", strs[i]);
-//            list.add(map);
-//
-//        }
-//
-//        return list;
-//    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list=new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("order_id", "10000000");
+        map.put("name", "shabi");
+        map.put("contact", "10086");
+        map.put("order_time", "2016-07-10 19:00");
+        list.add(map);
+
+        return list;
+    }
 
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
@@ -93,6 +78,4 @@ public class OrderFragment extends ListFragment {
 		Log.i(TAG, "--------onActivityCreated");
 
 	}
-
-
 }
