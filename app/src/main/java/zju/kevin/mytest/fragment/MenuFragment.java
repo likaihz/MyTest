e5a83a01b82eaa20166a4a887069e57b482d1d94
我叫李铁柱 <lk0219@foxmail.com>
2016-07-12 15:45:59 +0800

    july12-icon and btn listeners

--- app/src/main/java/zju/kevin/mytest/fragment/MenuFragment.java
+++ app/src/main/java/zju/kevin/mytest/fragment/MenuFragment.java
@@ -3,13 +3,16 @@ package zju.kevin.mytest.fragment;
 import zju.kevin.mytest.R;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,57 +22,51 @@ import java.util.Map;
 public class MenuFragment extends ListFragment {
 
     private String TAG = MenuFragment.class.getName();
+    private List<Map<String, Object>> data;
     /**
      * @描述 在onCreateView中加载布局
-     * */
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.i(TAG, "--------onCreateView");
-        return inflater.inflate(R.layout.menu_fragment, container,false);
+        return inflater.inflate(R.layout.menu_fragment, container, false);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final String [] from =new String[] {"name", "price","img"};
-        final int[] to = new int[] {R.id.dish_name, R.id.dish_price, R.id.dish_img};
+        final String[] from = new String[]{"name", "price", "img"};
+        final int[] to = new int[]{R.id.dish_name, R.id.dish_price, R.id.dish_img};
         Log.i(TAG, "--------onCreate");
 
-        SimpleAdapter adapter=new SimpleAdapter(this.getActivity(),
-                getData(), R.layout.menu_item, from, to);
-        this.setListAdapter(adapter);
+//        SimpleAdapter adapter=new SimpleAdapter(this.getActivity(),
+//                getData(), R.layout.menu_item, from, to);
+//        this.setListAdapter(adapter);
+        data=getData();
+        MenuItemAdapter adapter = new MenuItemAdapter(getActivity());
+        setListAdapter(adapter);
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-//        super.onListItemClick(l, v, position, id);
-//
-//        System.out.println(l.getChildAt(position));
-//        HashMap<String, Object> view= (HashMap<String, Object>) l.getItemAtPosition(position);
-//        System.out.println(view.get("title").toString()+"+++++++++title");
-//
-//
-//        Toast.makeText(getActivity(), TAG+l.getItemIdAtPosition(position), Toast.LENGTH_LONG).show();
-//        System.out.println(v);
-//        System.out.println(position);
-
-
+        System.out.println("Click On List Item!!!");
+        super.onListItemClick(l, v, position, id);
     }
 
 
     private List<Map<String, Object>> getData() {
-        List<Map<String ,Object>> list = new ArrayList<Map<String,Object>>();
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("name", "饺子");
         map.put("price", 20.00);
-        map.put("img",R.drawable.ic_launcher);
+        map.put("img", R.drawable.ic_launcher);
         list.add(map);
-        map=new HashMap<String, Object>();
+        map = new HashMap<String, Object>();
         map.put("name", "noodles");
         map.put("price", 100.00);
-        map.put("img",R.drawable.ic_launcher);
+        map.put("img", R.drawable.ic_launcher);
         list.add(map);
 
         return list;
@@ -82,5 +79,85 @@ public class MenuFragment extends ListFragment {
 
     }
 
+    static class ViewHolder {
+        public ImageView img;
+        public TextView price;
+        public TextView name;
+        public ImageView del;
+        public ImageView edit;
+    }
+    public class MenuItemAdapter extends BaseAdapter {
 
+        private LayoutInflater mInflater = null;
+        MenuItemAdapter(Context context){
+            //this.mInflater = LayoutInflater.from(context);
+            super();
+            mInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return pos;
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView == null){
+                holder = new ViewHolder();
+                //根据自定义的Item布局加载布局
+                convertView = mInflater.inflate(R.layout.menu_item, null);
+                holder.img = (ImageView)convertView.findViewById(R.id.dish_img);
+                holder.price = (TextView)convertView.findViewById(R.id.dish_price);
+                holder.name = (TextView)convertView.findViewById(R.id.dish_name);
+                holder.del = (ImageView)convertView.findViewById(R.id.delete_icon);
+                holder.edit = (ImageView)convertView.findViewById(R.id.edit_icon);
+                //将设置好的布局保存到缓存中，并将其设置在Tag里，以便后面方便取出Tag
+                convertView.setTag(holder);
+            }
+            else holder = (ViewHolder)convertView.getTag();
+
+            holder.img.setImageResource((Integer)data.get(position).get("img"));
+            holder.price.setText(String.valueOf(data.get(position).get("price")));
+            holder.name.setText((String)data.get(position).get("name"));
+            holder.del.setOnClickListener(new DelIconListener(position));
+            holder.edit.setOnClickListener(new EditIconListener(position));
+
+            return convertView;
+        }
+
+        class DelIconListener implements View.OnClickListener {
+            private int position;
+            DelIconListener(int pos) {position = pos;}
+            @Override
+            public void onClick(View view)
+            {
+                int vid=view.getId();
+                System.out.println("Click on del icon!");
+                Log.i("","Click on del icon!");
+            }
+        }
+
+        class EditIconListener implements View.OnClickListener {
+            private int position;
+            EditIconListener(int pos) {position = pos;}
+            @Override
+            public void onClick(View view)
+            {
+                int vid = view.getId();
+                System.out.println("Click on edit icon!");
+                Log.i("","Click on edit icon!");
+            }
+        }
+    }
 }
--- app/src/main/java/zju/kevin/mytest/fragment/OrderFragment.java
+++ app/src/main/java/zju/kevin/mytest/fragment/OrderFragment.java
@@ -3,13 +3,17 @@ package zju.kevin.mytest.fragment;
 import zju.kevin.mytest.R;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,6 +23,7 @@ import java.util.Map;
 public class OrderFragment extends ListFragment {
 
 	private String TAG = OrderFragment.class.getName();
+    private List<Map<String, Object>> data;
 	/**
 	 * @描述 在onCreateView中加载布局
 	 * */
@@ -33,12 +38,13 @@ public class OrderFragment extends ListFragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "--------onCreate");
-        String[] from=new String[] {"order_id", "name", "contact", "order_time"};
-        int[] to = new int[] {R.id.order_id, R.id.name, R.id.contact, R.id.order_time};
-		SimpleAdapter adapter=new SimpleAdapter(this.getActivity(),
-				getData(), R.layout.order_item, from, to);
-
-        this.setListAdapter(adapter);
+//        String[] from=new String[] {"order_id", "name", "contact", "order_time"};
+//        int[] to = new int[] {R.id.order_id, R.id.name, R.id.contact, R.id.order_time};
+        data = getData();
+//		SimpleAdapter adapter=new SimpleAdapter(this.getActivity(),
+//				getData(), R.layout.order_item, from, to);
+        OrderItemAdapter adapter = new OrderItemAdapter(getActivity());
+        setListAdapter(adapter);
 
 	}
 
@@ -78,4 +84,73 @@ public class OrderFragment extends ListFragment {
 		Log.i(TAG, "--------onActivityCreated");
 
 	}
+
+	static class ViewHolder {
+        public TextView order_id;
+        public TextView name;
+        public TextView contact;
+        public TextView order_time;
+        public Button btn;
+    }
+
+    class OrderItemAdapter extends BaseAdapter{
+        private LayoutInflater mInflater = null;
+        OrderItemAdapter(Context context){
+            //this.mInflater = LayoutInflater.from(context);
+            super();
+            mInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int pos) {
+            return pos;
+        }
+
+        @Override
+        public long getItemId(int pos) {
+            return pos;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                //根据自定义的Item布局加载布局
+                convertView = mInflater.inflate(R.layout.order_item, null);
+                holder.name = (TextView)convertView.findViewById(R.id.name);
+                holder.order_id = (TextView)convertView.findViewById(R.id.order_id);
+                holder.contact =(TextView)convertView.findViewById(R.id.contact);
+                holder.btn = (Button)convertView.findViewById(R.id.order_btn);
+                //将设置好的布局保存到缓存中，并将其设置在Tag里，以便后面方便取出Tag
+                convertView.setTag(holder);
+            }
+            else holder = (ViewHolder)convertView.getTag();
+
+            holder.order_id.setText((String)data.get(position).get("order_id"));
+            holder.name.setText((String)data.get(position).get("name"));
+            holder.contact.setText((String)data.get(position).get("contact"));
+            holder.order_time.setText((String)data.get(position).get("order_time"));
+            holder.btn.setOnClickListener(new OdrBtnListener(position));
+
+            return convertView;
+        }
+
+        class OdrBtnListener implements View.OnClickListener{
+            private int position;
+            OdrBtnListener(int pos) {position = pos;}
+            @Override
+            public void onClick(View view)
+            {
+                int vid = view.getId();
+                System.out.println("Click on edit btn!");
+                Log.i("","Click on edit btn!");
+            }
+        }
+    }
 }
